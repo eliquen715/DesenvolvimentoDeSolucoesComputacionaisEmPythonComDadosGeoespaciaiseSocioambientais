3f1c6335d854e43bb117226a8b517ea152df071c
Eliquen de Lara Krevelim <eliquen@alunos.utfpr.edu.br>
2026-06-01 12:28:32 -0300

    adicionado filtro de agua ao motor

--- Refatorado/motor.py
+++ Refatorado/motor.py
@@ -5,7 +5,7 @@ class ConexaoEE:
     @staticmethod
     def autenticar(service_account, key_path):
         try:
-            credentials = ee.ServiceAccountCredentials(service_account, key_path)#Autenticação API google earth engine
+            credentials = ee.ServiceAccountCredentials(service_account, key_path) # Autenticação API google earth engine
             ee.Initialize(credentials)
         except Exception as e:
             st.error(f"Erro ao inicializar: {e}")
@@ -29,3 +29,11 @@ class ConexaoEE:
     @staticmethod
     def calcular_ndvi(imagem):
         return imagem.normalizedDifference(['B8', 'B4'])
+
+    @staticmethod
+    def calcular_mndwi(imagem):
+        """
+        Calcula o Modified Normalized Difference Water Index (MNDWI) para o Sentinel-2.
+        Fórmula: (Verde - SWIR) / (Verde + SWIR) -> ['B3', 'B11']
+        """
+        return imagem.normalizedDifference(['B3', 'B11'])
